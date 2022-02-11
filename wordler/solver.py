61c6507d49aef5be0171647d4fe14417e212174e
Jakub Tesárek <jakub.tesarek@lmc.eu>
2022-02-11 22:43:02 +0100

    Bugfix: Solver no longer trying a present letter on the same position as before

--- wordler/solver.py
+++ wordler/solver.py
@@ -17,7 +17,7 @@ def get_words():
 
 class Solver:
     def __init__(self):
-        self.present_letters = set()
+        self.present_letters = {}
         self.positioned_letters = {}
         self.missing_letters = set()
         self.attempted_words = set()
@@ -39,7 +39,7 @@ class Solver:
     @property
     def encountered_letters(self):
         return set.union(
-            self.present_letters,
+            set(self.present_letters.keys()),
             self.missing_letters,
             set(self.positioned_letters.values())
         )
@@ -51,25 +51,29 @@ class Solver:
     def score_word(self, word, letter_frequency):
         score = 0
         for letter in self.unique_letters(word):
-            if letter not in self.encountered_letters:
-                score += letter_frequency[letter]
+            score += letter_frequency[letter]
         return score
 
     def matches_constraints(self, word):
         if word in self.attempted_words:
-            return False
+            return False  # tested before
 
         for letter in self.missing_letters:
             if letter in word:
-                return False
+                return False  # contains letter that is not present
             
-        for letter in self.present_letters:
+        for letter, positions in self.present_letters.items():
             if letter not in word:
-                return False
+                return False  # present letter is missing
+
+            for position in positions:
+                if word[position] == letter:
+                    return False  # present letter is on the same position
 
         for index, letter in self.positioned_letters.items():
             if word[index] != letter:
-                return False
+                return False  # missing letter on known position
+
         return True
 
 
@@ -100,6 +104,7 @@ class Solver:
             if occurence == '0':
                 self.missing_letters.add(letter)
             elif occurence == '1':
-                self.present_letters.add(letter)
+                tested_positions = self.present_letters.setdefault(letter, [])
+                tested_positions.append(index)
             else:
                 self.positioned_letters[index] = letter
